68f5caf1c561c072efdc568ba1ac4fcb5664aec5
Paweł Kobojek <PawelKobojek@gmail.com>
2016-01-14 19:25:05 +0100

    initial probability can now be passed as argument

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,10 +3,14 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         int gameSize = 10;
+        float initialLivingProbability = 0.5f;
         if (args.length > 0) {
             gameSize = Integer.parseInt(args[0]);
         }
-        long epochs = new GameOfLife(gameSize).run();
+        if (args.length > 1) {
+            initialLivingProbability = Float.parseFloat(args[1]);
+        }
+        long epochs = new GameOfLife(gameSize, initialLivingProbability).run();
         System.out.println("Population died after " + epochs + " epoch(s)");
     }
 }
